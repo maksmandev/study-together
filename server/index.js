61c0874eebebac45762ea8d72cdbd0c9d098825e
maksmandev <maksimkulibaba@gmail.com>
2019-03-02 20:35:10 +0200

    Add .env.example, debug module

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+const debug = require('debug')(process.env.DEBUG)
 
 app.get('/', (req, res) => {
+	debug('Default API working')
 	res.send('Hello World!')
 })
 
-app.listen(5000, () => {
-	console.log('App listening on port 5000!')
+app.listen(process.env.PORT, () => {
+	console.info('App listening on port 5000!')
 })
